5b7612fc792506fccb211c2f4fec5da2e84f0f4e
Maile Soon Fah <maile.soonfah23@ycdsbk12.ca>
2020-12-07 18:44:12 +0000

    updated problem1.py

--- problem1.py
+++ problem1.py
@@ -1,1 +1,20 @@
+'''
+-------------------------------------------------------------------------------
+Name:		problem1.py
+Purpose:	It converts the tempurature in celsius to be read in fahrenheit.
 
+Author:	Soon Fah.M
+
+Created:	date in 12/07/2020
+------------------------------------------------------------------------------
+'''
+
+
+# input for celsius
+c_Temp = float(input("Please input the tempurature in celsius: "))
+
+# converts celsius to fahrenheit
+f_Temp = float(c_Temp) * 9 / 5 + 32
+
+# prints the outcome telling the tempurature
+print("Your current tempurature is " + str(f_Temp) + " degrees fahrenheit.")
--- problem2.py
+++ problem2.py
@@ -1,1 +1,24 @@
+'''
+-------------------------------------------------------------------------------
+Name:		problem2.py
+Purpose:	Calculating chicken pieces to be distributed evenly among students in Mr.Fabroa's class and giving the remainder to Mr.Fabroa.
 
+Author: Soon Fah.M
+
+Created:	date in 12/07/2020
+------------------------------------------------------------------------------
+'''
+
+# input for the amount of chicken and the amount of students to calculate for 
+num_classmates = int(input("How many people are there in the class? (not including Mr.Fabroa): "))
+
+num_chicken = int(input("What is the number of chicken pieces: "))
+
+# calculating the amount of chicken everyone gets
+c_student_og = num_chicken / num_classmates
+c_teach = num_chicken % num_classmates
+c_student = c_student_og - c_teach
+
+# the result number of pieces for everyone
+print("There will be " + str(c_student) + " chicken pieces per student.")
+print("There will be " + str(c_teach) + " pieces of chicken for Mr.Fabroa.")
