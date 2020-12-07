c6530574ec9e13d621bc75b35e5327a6bfd32b63
Maile Soon Fah <maile.soonfah23@ycdsbk12.ca>
2020-12-07 18:53:22 +0000

    updated problem2.py

--- problem2.py
+++ problem2.py
@@ -9,7 +9,7 @@ Created:	date in 12/07/2020
 ------------------------------------------------------------------------------
 '''
 
-# input for the amount of chicken and the amount of students to calculate for 
+# input for the amount of chicken and the amount of students to calculate for
 num_classmates = int(input("How many people are there in the class? (not including Mr.Fabroa): "))
 
 num_chicken = int(input("What is the number of chicken pieces: "))
@@ -19,6 +19,6 @@ c_student_og = num_chicken / num_classmates
 c_teach = num_chicken % num_classmates
 c_student = c_student_og - c_teach
 
-# the result number of pieces for everyone
+# the end result of distributed chicken
 print("There will be " + str(c_student) + " chicken pieces per student.")
 print("There will be " + str(c_teach) + " pieces of chicken for Mr.Fabroa.")
